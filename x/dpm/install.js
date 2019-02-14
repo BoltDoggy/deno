104c929ed0100034366a7fac3194657e5e336e0a
🐶Bolt <bolt@wangchunyandeMacBook-Pro-4.local>
2019-02-15 00:51:24 +0800

    update

--- x/dpm/install.js
+++ x/dpm/install.js
@@ -1,12 +1,15 @@
-import { writeFileSync, env, chmodSync } from "deno";
+import { writeFileSync, env, chmodSync, args } from "deno";
+
+const url = 'https://deno.boltdoggy.com/x/dpm/mod.js';
+const name = 'dpm';
 
 const ENV = env();
 
 const encoder = new TextEncoder();
 const data = encoder.encode(`#!/bin/sh
-deno https://deno.boltdoggy.com/x/dpm/mod.js "$@"
+deno ${url} "$@"
 `);
 
-const dvmFilePath = `${ENV.HOME}/.deno/bin/dvm`;
+const dvmFilePath = `${ENV.HOME}/.deno/bin/${name}`;
 writeFileSync(dvmFilePath, data);
 chmodSync(dvmFilePath, 0o777);
